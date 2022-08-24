8a9f64361cce0c928daf9a9f3e46af43d49a4afd
Niraj Kumar <niraj.kumar@brigosha.com>
2022-08-24 17:45:44 +0530

    Initial Commit

---
+++ table view with data passing and delete/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  table view with data passing and delete
+//
+//  Created by Niraj on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
